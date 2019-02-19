6b9f67fed3e341d6cb4c2b054d0d80962868949e
Stuart Fooks <sb.fooks@gmail.com>
2019-02-19 16:08:29 -0500

    delete method working

--- dog-park/src/Show/Show.js
+++ dog-park/src/Show/Show.js
@@ -11,6 +11,8 @@ class Show extends Component {
     this.state = {
       park: {}
     }
+
+    this.deleteHandler = this.deleteHandler.bind(this)
   }
 
 componentDidMount() {
@@ -23,21 +25,23 @@ componentDidMount() {
         .catch(err => {
         console.log(err)
         })
+}
 
-    axios.delete(url + this.props.match.params.id)
-        .then(park => {
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+deleteHandler() {
+  axios.delete(url + this.props.match.params.id)
+  .then(park => {
+      console.log(park.data);
+  })
+  .catch((err) => {
+      console.log(err);
+  })
 }
 
   render () {
 
     console.log(this.state.park)
         return (
-          <div className="park" key={this.match.props.park._id}>
+          <div className="park" key={this.state.park._id}>
             <h1>{this.state.park.name}</h1>
             <h3>Amenities:</h3>
             <ul>
@@ -48,6 +52,7 @@ componentDidMount() {
               <li>Up Votes: {this.state.park.upVotes}</li>
               <li>Down Votes: {this.state.park.downVotes}</li>
             </ul>
+            <button onClick={this.deleteHandler} name={this.state.park._id}>Delete</button>
           </div>
         )
     }
