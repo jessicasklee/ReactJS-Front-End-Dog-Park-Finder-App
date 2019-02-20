3180421becbe85c233f03391f835d2649f33aaf7
Stuart Fooks <sb.fooks@gmail.com>
2019-02-20 14:09:50 -0500

    log in and sign up functional

--- dog-park/src/App/App.js
+++ dog-park/src/App/App.js
@@ -1,24 +1,88 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Link, Switch } from 'react-router-dom'
+import axios from 'axios'
 import Home from '../Home/Home'
 import Create from '../Create/Create'
 import Show from '../Show/Show';
+import SignUp from '../SignUp/SignUp'
+import LogIn from '../LogIn/LogIn'
+
+const url = 'http://localhost:8080'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      email: "",
+      password: "",
+      isLoggedIn: false
+    }
+
+    this.inputChanger = this.inputChanger.bind(this)
+    this.signupSubmit = this.signupSubmit.bind(this)
+    this.loginSubmit = this.loginSubmit.bind(this)
+    this.logoutSubmit = this.logoutSubmit.bind(this)
+  }
+
+  inputChanger(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  signupSubmit(e) {
+    e.preventDefault()
+    axios.post(url + "/users/signup", {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then(res => {
+      localStorage.token = res.data.token
+      this.setState({ isLoggedIn: true })
+    })
+    .catch(err => console.log(err))
+  }
+
+  loginSubmit(e) {
+    e.preventDefault()
+    axios.post(url + "/users/login", {
+      email: this.state.email,
+      password: this.state.password
+    })
+    .then(res => {
+      localStorage.token = res.data.token
+
+      this.setState({
+        isLoggedIn: true
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  logoutSubmit() {
+    this.setState({
+      email: '',
+      password: '',
+      isLoggedIn: false
+    })
+    localStorage.clear()
+  }
+
   render() {
-
-
     return (
       <div className="App">
         <header>
           <Link to="/"><h1>Dog Park Finder</h1></Link>
           <nav>
             <Link to="/create"><h3>New Park</h3></Link>
+            <Link to="/signup"><h3>Sign Up</h3></Link>
+            <Link to="/login"><h3>Log In</h3></Link>
           </nav>
         </header>
         <Switch>
           <Route path="/create" component={Create} />
+          <Route path="/signup" render={() => <SignUp inputChanger={this.inputChanger} signupSubmit={this.signupSubmit} />} />
+          <Route path="/login" render={() => <LogIn inputChanger={this.inputChanger} loginSubmit={this.loginSubmit} />} />
           <Route path="/:id" component={Show} />
           <Route path="/" component={Home} />
         </Switch>
