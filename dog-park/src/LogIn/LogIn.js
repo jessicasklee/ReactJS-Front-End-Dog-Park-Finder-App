670637a6d5331bfe0b155102f0cef3aaf9d72812
Lindsay Urchyk <urchykli@gmail.com>
2019-02-22 11:21:42 -0500

    styled login and signup forms for mobile

--- dog-park/src/LogIn/LogIn.js
+++ dog-park/src/LogIn/LogIn.js
@@ -24,10 +24,14 @@ class LogIn extends Component {
         <h1>Log In</h1>
         <div className="form-container">
           <form onSubmit={this.props.loginSubmit}>
+          <p>
             <label>Email:</label>
-            <input name="email" type="text" onChange={this.props.inputChanger} />
+            <input className="loginInput"  name="email" type="text" onChange={this.props.inputChanger} />
+            </p>
+            <p>
             <label>Password:</label>
-            <input name="password" type="password" onChange={this.props.inputChanger}/>
+            <input className="loginInput" name="password" type="password" onChange={this.props.inputChanger}/>
+            </p>
             <input type="submit" value="Submit"></input>
           </form>
         </div>
